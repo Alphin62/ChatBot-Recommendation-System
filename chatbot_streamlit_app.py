cfcc8b5f2c6d459b8f53257caddd265ff8a677c3
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-02 23:25:25 +0530

    Update chatbot_streamlit_app.py

--- chatbot_streamlit_app.py
+++ chatbot_streamlit_app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-st.title('Chatbot & Recommendation')
+st.title('Chatbot & Recommendation System')
 st.image('Chatbot_image.jpg')
 st.write("""
          ## About
          
-         A chatbot is a computer program that uses artificial intelligence (AI) and natural language processing (NLP) to understand customer questions and automate responses to them, simulating human conversation.
-         
-         ### By Alphin Gnanaraj I
+         ### _*By Alphin Gnanaraj I
          """)
 
