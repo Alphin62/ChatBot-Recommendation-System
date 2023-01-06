6e88bdaf6c95f93b6a58caf564b99ec250a0c216
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-06 20:10:25 +0530

    Update Chatbot_streamlit_app.py

--- Chatbot_streamlit_app.py
+++ Chatbot_streamlit_app.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import streamlit as st
 import json
 import pickle
@@ -18,20 +12,7 @@ with open("intents.json", "r") as f:
 # Load the trained model from a file
 with open("chatbot_model.pkl", "rb") as f:
     model = pickle.load(f)
-
-def chatbot_ui():
-    message = st.text_input("Enter your message:")
-    if message:
-        response = ChatBot.get_response(message, model, intents)
-        st.write(f"Chatbot: {response}")
-
-st.title("Chatbot")
-chatbot_ui()
-
-
-# In[3]:
-
-
+    
 def get_response(message, model, intents):
     # Use the model to predict the class of the input message
     classification = model.predict(message)
@@ -44,9 +25,11 @@ def get_response(message, model, intents):
     # Return a default response if no matching intent is found
     return "I'm sorry, I didn't understand your message."
 
+def chatbot_ui():
+    message = st.text_input("Enter your message:")
+    if message:
+        response = ChatBot.get_response(message, model, intents)
+        st.write(f"Chatbot: {response}")
 
-# In[ ]:
-
-
-
-
+st.title("Chatbot")
+chatbot_ui()
