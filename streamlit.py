def608cd5af45a798df5575d00487fcf8bc67166
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-10 01:34:28 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import streamlit as st
+import pickle
+import nltk
+import numpy as np
+import json
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[4]:
+
+
+# load the model from file
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# load the intents file
+with open('intents.json') as file:
+    data = json.load(file)
+
+
+# In[6]:
+
+
+def bag_of_words(sentence, words):
+    # lemmatize and lower the words in the sentence
+    sentence_words = [lemmatizer.lemmatize(w.lower()) for w in nltk.word_tokenize(sentence)]
+
+    bag = [0 for _ in range(len(words))]
+
+    for sw in sentence_words:
+        for i, w in enumerate(words):
+            if w == sw:
+                bag[i] = 1
+            
+    return bag
+
+
+# In[7]:
+
+
+st.title("Chatbot & Recommendation Model")
+
+sentence = st.text_input("Enter a sentence:")
+
+if sentence:
+    prob_result = model.predict_proba(np.array([bag_of_words(sentence, words)]))[0]
+    index = np.argmax(prob_result)
+    tag = labels[index]
+    probability = prob_result[index]
+
+    if probability > 0.7:
+        for tg in data['intents']:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+        st.success(f"Intent: {tag} ({probability:.2f})\n\nResponse: {random.choice(responses)}")
+    else:
+        st.error("Unable to classify the intent with high confidence. Please try a different sentence.")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
