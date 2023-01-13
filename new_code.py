4053e248363b0d2b31376c2a996fe46da12b6acc
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-13 18:35:29 +0530

    Update new_code.py

--- new_code.py
+++ new_code.py
@@ -8,15 +8,21 @@ with open('intents.json') as f:
 # Define a function to handle chatbot interactions
 
 def chatbot():
-    #st.set_page_config(page_title="My Chatbot", page_icon=":guardsman:", layout="wide")
+    
+    st.set_page_config(page_title="My Chatbot", page_icon=":guardsman:", layout="wide", 
+                       title_widget=lambda: st.markdown("<font face='Times New Roman'>My Chatbot</font>"))
+    
     st.title("_*My Chatbot*_")
+    st.image('Chatbot_image.jpg')
     
     user_input = st.text_input("What can I help you with?")
         # Loop through intents to find a match
+        
     for intent in intents:
         if user_input in intent["inputs"]:
             st.write(intent["response"])
             return
+        
         # If no match is found
     st.write("I'm sorry, I didn't understand your input.")        
         
