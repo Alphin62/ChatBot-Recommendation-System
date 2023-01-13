3f45c2fe72f249f6b0c3285ddd26aecb0eb4ac93
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-13 17:51:28 +0530

    Update new_code.py

--- new_code.py
+++ new_code.py
@@ -1,37 +1,26 @@
-import streamlit as st 
-from nltk.chat.util import Chat, reflections
-#from pyjokes import get_joke
+import json
+import streamlit as st
 
+# Load intents data from json file
+with open('intents.json') as f:
+    intents = json.loads(f.read())
 
-pairs = [
-    
-         ['my name is (.*)', ['Hi! Hello %1 Sir. I am JARVIS. How may i Help You?']],
-         ['hi', ['Hi Hello I am your JARVIS. How can I Help you?????']],
-         ['What can you Do', ['Yes Sir, I can Do many Tasks.']],
-        #  ['open (.*)', ['Sorry Sir, I cant open %1 .I am still a Chatbot. I cant automate things.', webbrowser.open('%1')]],
-         ['do you think there is a creator', ['Yes, According to me there is creator of all this because even I have been created by Abdul Rehman']],
-         ['about you', ['I am a ChatBot created by Prof Abdul Rehman']],
-         ['Who am i', ['please try typing "my name is --"']],
-         ['(Tell me a joke|Joke|joke|tell me jokes)', ['Hehe Do you know its Saturday Today, Elephant has two Teeths, We humans have Four legs']],
-         #['Abd JOke', [get_joke(language='en', category='neutral') ]],
-         ['Who created you?', ['Abdul Rehman has created me.']],
-            
-]
+# Define a function to handle chatbot interactions
+def chatbot():
+    # Get user input
+    user_input = st.text_input("What can I help you with?")
 
+    # Loop through intents to find a match
+    for intent in intents:
+        if user_input in intent["inputs"]:
+            st.write(intent["response"])
+            return
 
-st.title("Rule based Chatbot")
-st.subheader("This is a Rule based Chatbot made using NlTK and Python by Abdul Rehman ")
+    # If no match is found
+    st.write("I'm sorry, I didn't understand your input.")
 
-
-def main():
-    st.write("Initialize the Chat bot By Typing Hi ")
-    ref = st.text_input("Start your chat here")
-
-    # a = st.text_input("Initialize your Conversation By Typing Hi")
-    # chat.converse()
-    chat = Chat(pairs, reflections)
-    respo = chat.respond(ref)
-    st.write(respo)
-
-if __name__=="__main__":
-    main()
+# Run the Streamlit app
+if __name__ == "__main__":
+    st.set_page_config(page_title="My Chatbot", page_icon=":guardsman:", layout="wide")
+    st.title("My Chatbot")
+    chatbot()
