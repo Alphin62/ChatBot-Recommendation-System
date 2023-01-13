6b8c35a565f6b02a7b9bf8a1294d10f6d655c483
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-13 18:14:01 +0530

    Update new_code.py

--- new_code.py
+++ new_code.py
@@ -10,15 +10,15 @@ with open('intents.json') as f:
 def chatbot():
     st.set_page_config(page_title="My Chatbot", page_icon=":guardsman:", layout="wide")
     st.title("My Chatbot")
-    if st.button("Submit"):
-        user_input = st.text_input("What can I help you with?")
+    
+    user_input = st.text_input("What can I help you with?")
         # Loop through intents to find a match
-        for intent in intents:
-            if user_input in intent["inputs"]:
-                st.write(intent["response"])
-                return
+    for intent in intents:
+        if user_input in intent["inputs"]:
+            st.write(intent["response"])
+            return
         # If no match is found
-        st.write("I'm sorry, I didn't understand your input.")        
+    st.write("I'm sorry, I didn't understand your input.")        
         
         
 # Run the Streamlit app
