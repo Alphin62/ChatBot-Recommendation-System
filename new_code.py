758faa168cc32073a016c13f4875b83f910e7eaa
Alphin Gnanaraj I <118610159+Alphin62@users.noreply.github.com>
2023-01-14 01:59:05 +0530

    Update new_code.py

--- new_code.py
+++ new_code.py
@@ -10,7 +10,14 @@ with open('intents.json') as f:
 def chatbot():
     
     st.title("_*My Chatbot & Recommendation System*_")
-    st.image('Chatbot_image.jpg')
+    st.write("""
+         ## About
+         A chatbot is a computer program that is designed to simulate conversation with human users. It uses natural language processing (NLP) and machine learning (ML) technologies to understand and respond to user inputs. Chatbots can be integrated into various platforms such as websites, mobile apps, and messaging apps, and can be used to perform a wide range of tasks such as answering questions, providing customer support, and even completing transactions. They are becoming increasingly popular as they can save time and resources for businesses and provide 24/7 availability for users. Essentially, chatbot is a computer program that can chat with people in a way similar to a human conversation.
+         
+         *** Rule Based Approach : *** A rule-based chatbot is a type of computer program that uses pre-defined rules to determine how to respond to users' inputs based on keywords or phrases, it's a simple chatbot that can handle a limited set of tasks and can't understand more complex or nuanced inputs.
+         
+         _**By Alphin Gnanaraj I**_
+         """)
     
     user_input = st.text_input("Type your text below")
         # Loop through intents to find a match
@@ -22,7 +29,7 @@ def chatbot():
         
         # If no match is found
     st.write("Sorry, I didn't understand your input.")        
-        
+    st.image('Chatbot_image.jpg')    
         
 # Run the Streamlit app
 if __name__ == "__main__":
